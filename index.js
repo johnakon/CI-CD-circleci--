3b16623b9934ac22a8b23b2a3cd5f341e19f19ef
John <john.akonya@gmail.com>
2020-02-27 12:55:00 +0300

    made changes

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, "./public");
+app.use(express.static(publicDir));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 const marks = require("./models/marksModel");
 const student = require("./models/studentModel");
 
@@ -9,3 +20,8 @@ const johnGrade = new marks("Akonya", "P.E", 70);
 // johnGrade.computeScore();
 
 module.exports = johnGrade;
+
+//port
+app.listen(port, () => {
+  console.log("up at " + port);
+});
